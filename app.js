e1d643e64b5702ed8f9e981b5bd8e7a68655cf8b
Mayank Srivastava <sriv.mayank@gmail.com>
2013-08-05 10:42:12 +0530

    Function comments
    Its always a good idea to provide your functions with some comments.

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 var App = function () {
 
-	var jsonData = [{"id":1},{"id":2},{"id":3}];
+	var jsonData1 = [{"id":1},{"id":2},{"id":3}];
 	
-	var jsonData1 = [{"id":1},{"id":2},{"id":3,"children":[{"id":4},{"id":5},{"id":6}]}];
+	var jsonData2 = [{"id":1},{"id":2},{"id":3,"children":[{"id":4},{"id":5},{"id":6}]}];
 	
-	var jsonData2 = [{"id":1},{"id":2,"children":[{"id":3,"children":[{"id":9, "children":[{"id":10}]}]}]},{"id":4,"children":[{"id":5},{"id":6},{"id":7, "children": [{"id":11}, {"id":12}]}]}];
+	var jsonData3 = [{"id":1},{"id":2,"children":[{"id":3,"children":[{"id":9, "children":[{"id":10}]}]}]},{"id":4,"children":[{"id":5},{"id":6},{"id":7, "children": [{"id":11}, {"id":12}]}]}];
 	
 	var tpl1 = Handlebars.compile($("#tpl1").html());
 	
 	var tpl2 = Handlebars.compile($("#tpl2").html());
 	
-	var terry = function(data, id){
+	/**
+	 * Render calls specific template functions based on
+	 * whether an object has children or not. 
+	 */
+	var render = function(data, id){
 		for(var i=0; i<data.length; i++){
 			if(data[i].children==undefined){
-				//compile template tpl1
-				ferry(data[i], id);
+				//compile template tpl1 for li element
+				createLiEl(data[i], id);
 			}
 			else{
-				//compile template 1 tpl1
-				ferry(data[i], id);
-				//compile template 2 tpl2for ul
-				merry(data[i], id);
-				terry(data[i].children, data[i].id);
+				//compile template tpl1 for li element
+				createLiEl(data[i], id);
+				//compile template tpl2 for ul element
+				createUlEl(data[i], id);
+				render(data[i].children, data[i].id);
 			}
 		}
 	};
 	
-	var	ferry = function(data, id){
+	/**
+	 * Creates LI element to be added to list.
+	 * id specifies whether this element goes to root or inside another element. 
+	 */
+	var	createLiEl = function(data, id){
 		var html    = tpl1(data);
 		if(id==0){
 			$("#here").append(html);
@@ -37,7 +45,11 @@ var App = function () {
 		}
 	};
 	
-	var	merry = function(data, id){
+	/**
+	 * Create UL element to be added to list.
+	 * id specifies whether this element goes to root or inside another element.
+	 */
+	var	createUlEl = function(data, id){
 		var html    = tpl2(data);
 		if(id==0){
 			$("#here").append(html);
@@ -50,7 +62,7 @@ var App = function () {
 	
     return {
     	init: function(){
-    		terry(jsonData2, 0);
+    		render(jsonData2, 0);
     	}
     };
 
